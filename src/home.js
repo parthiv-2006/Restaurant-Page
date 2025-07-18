4b7ab8936c0a9793e998421b9c53d0d3f5a5458e
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-07-18 16:38:51 -0400

    Remove initial welcome message

--- src/home.js
+++ src/home.js
@@ -10,13 +10,13 @@ export default function loadHomePage() {
     img.alt = "Restaurant Image"
     img.src = RestaurantImage
 
-    const displayMessage = document.createElement('h1')
-    displayMessage.textContent = "Welcome to Paul's Cafe"
+    // const displayMessage = document.createElement('h1')
+    // displayMessage.textContent = "Welcome to Paul's Cafe"
 
     const description = document.createElement('p')
     description.textContent = "Placeholder for Restaurant Description"
 
-    homeDiv.appendChild(displayMessage)
+    // homeDiv.appendChild(displayMessage)
     homeDiv.appendChild(img)
     homeDiv.appendChild(description)
 
